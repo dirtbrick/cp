e0eccf0013b737ae461afb0efd1ca9bf689e83dc
dirtbrick <anabsolutelyobviouslyfakemail@gmail.com>
2023-05-29 19:25:03 +0800

    modified:   graph_theory/rat_maze_problem.py

--- graph_theory/rat_maze_problem.py
+++ graph_theory/rat_maze_problem.py
@@ -1,5 +1,7 @@
 from typing import List
 
+# helps print the 2D List : matrix
+xprint = lambda matrix: [[[print(item,end=" ") for item in rows],print()] for rows in matrix]
 def mprint(
     matrix: List[List[int]]
 ) -> None:
@@ -7,17 +9,19 @@ def mprint(
         for item in rows:
             print(item,end=" ")
         print()
-        
-isReached = False  
 
+# right-down only solution to the maze        
+isReached = False  
 def final_solve(
     maze: List[List[int]], 
     solution: List[List[int]],
     x: int = 0,
-    y: int = 0
+    y: int = 0,
+    xf: int = 3,
+    yf: int = 3
 ) -> List[List[int]]: 
     global isReached
-    if y == 3 and x == 3:
+    if y == yf and x == xf:
         solution[y][x] = 1
         isReached = True
         return solution
@@ -25,27 +29,28 @@ def final_solve(
         if y < len(maze) and x+1 < len(maze):
             if maze[y][x+1] == 1 and isReached == False:
                 solution[y][x] = 1
-                final_solve(maze,solution,x+1,y)
+                final_solve(maze,solution,x+1,y,xf,yf)
         if y+1 < len(maze) and x < len(maze):
             if maze[y+1][x] == 1 and isReached == False:
                 solution[y][x] = 1
-                final_solve(maze,solution,x,y+1)
+                final_solve(maze,solution,x,y+1,xf,yf)
         if isReached == False:
             solution[y][x] = 0
-          
-def printer(x,y,grid): 
+            
+# Prints the coordinates where the solution is added / removed          
+def printer(x,y,grid,xf=3,yf=3): 
     global isReached
     print("X: {}, Y: {}, G[Y][X]: {}".format(x,y,grid[y][x]))  
-    if y == 3 and x == 3:
+    if y == yf and x == xf:
         isReached = True
         print("Reached End")
-    else:
+    elif isReached == False:
         if y < len(grid) and x+1 < len(grid):
             if grid[y][x+1] == 1 and isReached == False:
-                printer(x+1,y,grid)
+                printer(x+1,y,grid,xf=xf,yf=yf)
         if y+1 < len(grid) and x < len(grid):
             if grid[y+1][x] == 1 and isReached == False:
-                printer(x,y+1,grid)
+                printer(x,y+1,grid,xf=xf,yf=yf)
         if isReached == False:
             print("Reached Else: x: {}, y: {}".format(x,y))          
               
@@ -58,7 +63,7 @@ def main():
     mprint(maze_matrix)
     print()
     final_solve(maze_matrix,empty_matrix)
-    #printer(0,0,maze_matrix)
+    #printer(0,0,maze_matrix,xf=len(maze_matrix)-2,yf=len(maze_matrix)-1)
     print()
     mprint(empty_matrix)
     
