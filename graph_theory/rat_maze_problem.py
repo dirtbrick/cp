6e517ca80b6d9e31204996236238ebf0d2009d4d
dirtbrick <anabsolutelyobviouslyfakemail@gmail.com>
2023-05-29 18:59:56 +0800

    added rat_maze

--- graph_theory/rat_maze_problem.py
+++ graph_theory/rat_maze_problem.py
@@ -1,54 +1,67 @@
-import typing
-maze_matrix = [[1, 0, 0, 0],
-               [1, 1, 1, 1],
-               [0, 1, 0, 0],
-               [1, 1, 1, 1]]
+from typing import List
 
-solution_matrix = [[0 for x in range(len(maze_matrix[0]))]for y in range(len(maze_matrix))]
-x = 1
-y = 0
-# cur = (y,x), end = (y,x)
-def recur_solve(coord: typing.List[int], endpt: int) -> bool:
-    try:
-        right = maze_matrix[coord[y]][coord[x]+1]
-    except:
-        right = 0
-    try:
-        down = maze_matrix[coord[y]+1][coord[x]]
-    except:
-        down = 0
-    try:
-        solution_matrix[coord[y]][coord[x]] = 1
-    except:
-        pass
-    print("(y,x):",coord[y],coord[x])
-    print("r:",right)
-    print("d:",down)
-    isDead = True if right == 0 and down == 0 else False
-    if isDead:
-        print("Dead at:",coord[y],coord[x])
-        try:
-            solution_matrix[coord[y]][coord[x]] = 0
-        except:
-            return False
-    elif right == 1:
-        if not recur_solve((coord[y],coord[x]+1),endpt):
-            return recur_solve((coord[y]+1,coord[x]),endpt)
-    elif down == 1:
-        return recur_solve((coord[y]+1,coord[x]),endpt)
-    
-def mprint(matrix: typing.List[typing.List[int]]) -> None:
+def mprint(
+    matrix: List[List[int]]
+) -> None:
     for rows in matrix:
         for item in rows:
             print(item,end=" ")
         print()
-                    
+        
+isReached = False  
+
+def final_solve(
+    maze: List[List[int]], 
+    solution: List[List[int]],
+    x: int = 0,
+    y: int = 0
+) -> List[List[int]]: 
+    global isReached
+    if y == 3 and x == 3:
+        solution[y][x] = 1
+        isReached = True
+        return solution
+    else:
+        if y < len(maze) and x+1 < len(maze):
+            if maze[y][x+1] == 1 and isReached == False:
+                solution[y][x] = 1
+                final_solve(maze,solution,x+1,y)
+        if y+1 < len(maze) and x < len(maze):
+            if maze[y+1][x] == 1 and isReached == False:
+                solution[y][x] = 1
+                final_solve(maze,solution,x,y+1)
+        if isReached == False:
+            solution[y][x] = 0
+          
+def printer(x,y,grid): 
+    global isReached
+    print("X: {}, Y: {}, G[Y][X]: {}".format(x,y,grid[y][x]))  
+    if y == 3 and x == 3:
+        isReached = True
+        print("Reached End")
+    else:
+        if y < len(grid) and x+1 < len(grid):
+            if grid[y][x+1] == 1 and isReached == False:
+                printer(x+1,y,grid)
+        if y+1 < len(grid) and x < len(grid):
+            if grid[y+1][x] == 1 and isReached == False:
+                printer(x,y+1,grid)
+        if isReached == False:
+            print("Reached Else: x: {}, y: {}".format(x,y))          
+              
 def main():
-    recur_solve([0,0],[3,3])
-    print()
+    maze_matrix =  [[1, 0, 0, 0],
+                    [1, 1, 1, 1],
+                    [0, 1, 0, 0],
+                    [1, 1, 1, 1]]
+    empty_matrix = [[0 for x in range(len(maze_matrix[0]))] for y in range(len(maze_matrix))]
     mprint(maze_matrix)
     print()
-    mprint(solution_matrix)
+    final_solve(maze_matrix,empty_matrix)
+    #printer(0,0,maze_matrix)
+    print()
+    mprint(empty_matrix)
+    
 
 if __name__ == "__main__":
     main()
